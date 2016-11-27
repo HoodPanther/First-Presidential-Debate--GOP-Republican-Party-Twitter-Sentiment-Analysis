b56b9bb3a9cdef8229e4378b511d2e6716087207
Itrikha <trikha.ishan@gmail.com>
2016-11-27 14:52:46 -0500

    Update FinalProject.R
    Comparison Cloud of Donald Trump based on sentiments.
    Copied script from Usman Khaliq (Kaggle Dataset, The Donald Cloud).

--- FinalProject.R
+++ FinalProject.R
@@ -1,6 +1,13 @@
+install.packages('RColorBrewer')
 library('readr')
 library('dplyr')
 library('igraph')
+library(ggplot2) # Data visualization
+library(readr) # CSV file I/O, e.g. the read_csv function
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer") 
 readcsv<- read_csv('Sentiment_GOP.csv')
 #Normalizing data by removing NA values
 readcsv_normalized<- readcsv[!(is.na(readcsv$tweet_location) | readcsv$tweet_location=="" |  readcsv$subject_matter=="None of the above" | is.na(readcsv$candidate)) , ]
@@ -83,4 +90,113 @@ V(state_graph_racialissues_sentiments)$vertex_degree <-  degree(state_graph_raci
 degree(state_graph_racialissues_sentiments)
 plot(state_graph_racialissues_sentiments, layout=layout.fruchterman.reingold,vertex.size=V(state_graph_racialissues_sentiments)$vertex_degree)
 
+
+degree(state_graph_racialissues_sentiments)
+degree.distribution(state_graph_racialissues_sentiments)
+#Histogram of candidates for a specific issue having positive tweets 
+
+
+
+#load the twitter data
+#format the tweets 
+readcsv$text <- gsub("#GOPDebate", "", readcsv$text) 
+readcsv$text = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", readcsv$text) 
+readcsv$text <- gsub("#GOPdebate", "", readcsv$text)  
+readcsv$text = gsub("[[:punct:]]", "", readcsv$text)
+readcsv$text = gsub("[[:digit:]]", "",readcsv$text)
+readcsv$text = gsub("http\\w+", "", readcsv$text)
+readcsv$text = gsub("[ \t]{2,}", "", readcsv$text)
+readcsv$text = gsub("^\\s+|\\s+$", "", readcsv$text)
+readcsv$text = gsub("amp", "", readcsv$text)
+readcsv$text = gsub("[^\x20-\x7E]", "", readcsv$text)
+
+
+
+#create data frame containing text, sentiment and retweet count
+tweet_data <- subset(readcsv,selec=c("text","sentiment","retweet_count")) 
+
+sents = levels(factor(tweet_data$sentiment))  
+cand=levels(factor(tweet_data$candidate))
+
+
+labels <- lapply(sents, 
+                 function(x,y) paste(x,format(
+                round((length((tweet_data[tweet_data$sentiment ==x ,])$text)/
+                         length(tweet_data$sentiment)*100),2),nsmall=2),"%"))
+
+nemo = length(sents)
+emo.docs = rep("", nemo) 
+
+for (i in 1:nemo)
+{
+  tmp = tweet_data[tweet_data$sentiment == sents[i],]$text
+  
+  emo.docs[i] = paste(tmp,collapse=" ")
+}  
+
+emo.docs = removeWords(emo.docs, stopwords("german"))
+
+emo.docs = removeWords(emo.docs, stopwords("english"))
+
+corpus = Corpus(VectorSource(emo.docs)) 
+tdm = TermDocumentMatrix(corpus)
+tdm = as.matrix(tdm)
+colnames(tdm) = labels
+
+# comparison word cloud
+comparison.cloud(tdm, colors = brewer.pal(nemo, "Dark2"),
+                 scale = c(3,.5), random.order = FALSE, title.size = 1.5)
+
+
+# world Cloud Sentiments of people on Donald Trump tweets.
+
+readcsv_normalized_DT<-readcsv_normalized[readcsv_normalized$candidate=="Donald Trump",]
+
+readcsv_normalized_DT$text <- gsub("#GOPDebate", "", readcsv_normalized_DT$text) 
+readcsv_normalized_DT$text = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", readcsv_normalized_DT$text) 
+readcsv_normalized_DT$text <- gsub("#GOPdebate", "", readcsv_normalized_DT$text)  
+readcsv_normalized_DT$text = gsub("[[:punct:]]", "", readcsv_normalized_DT$text)
+readcsv_normalized_DT$text = gsub("[[:digit:]]", "",readcsv_normalized_DT$text)
+readcsv_normalized_DT$text = gsub("http\\w+", "", readcsv_normalized_DT$text)
+readcsv_normalized_DT$text = gsub("[ \t]{2,}", "", readcsv_normalized_DT$text)
+readcsv_normalized_DT$text = gsub("^\\s+|\\s+$", "", readcsv_normalized_DT$text)
+readcsv_normalized_DT$text = gsub("amp", "", readcsv_normalized_DT$text)
+readcsv_normalized_DT$text = gsub("[^\x20-\x7E]", "", readcsv_normalized_DT$text)
+
+
+
+#create data frame containing text, sentiment and retweet count
+tweet_data <- subset(readcsv_normalized_DT,selec=c("text","sentiment")) 
+
+sents = levels(factor(tweet_data$sentiment))  
+cand=levels(factor(tweet_data$candidate))
+
+
+labels <- lapply(sents, 
+                 function(x) paste(x,format(
+                   round((length((tweet_data[tweet_data$sentiment ==x ,])$text)/
+                            length(tweet_data$sentiment)*100),2),nsmall=2),"%"))
+
+nemo = length(sents)
+emo.docs = rep("", nemo) 
+
+for (i in 1:nemo)
+{
+  tmp = tweet_data[tweet_data$sentiment == sents[i],]$text
+  
+  emo.docs[i] = paste(tmp,collapse=" ")
+}  
+
+emo.docs = removeWords(emo.docs, stopwords("german"))
+
+emo.docs = removeWords(emo.docs, stopwords("english"))
+
+corpus = Corpus(VectorSource(emo.docs)) 
+tdm = TermDocumentMatrix(corpus)
+tdm = as.matrix(tdm)
+colnames(tdm) = labels
+
+# comparison word cloud
+comparison.cloud(tdm, colors = brewer.pal(nemo, "Accent"),
+                 scale = c(3,.5), random.order = FALSE, title.size = 1.5)
 
